69e4703b6f2e043c4b5f1d87c007881da06eed23
hawabozer <hawaabozer@gmail.com>
2022-07-01 07:16:32 -0700

    change code

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,3 +1,6 @@
 INSERT INTO Department(Name,CollgID,Code)
 VALUES ('Computer Network',1,'CN');
 SELECT * FROM Department
+insert into department(Name,collgID,code)
+VALUES ('Computer Hardwaer',1,'CE');
+
