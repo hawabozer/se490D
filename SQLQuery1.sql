1e237f444f1083410ec8f20578295afc2dc09f43
safaaibrah <safaa200036@gmail.com>
2022-07-02 10:55:23 +0200

    fff

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,6 +1,6 @@
 INSERT INTO Department(Name,CollgID,Code)
-VALUES ('Computer Network',1,'CN');
+VALUES ('software enginerr',1,'CN');
 SELECT * FROM Department
 insert into department(Name,collgID,code)
-VALUES ('Computer Hardwaer',1,'CE');
+VALUES ('software enginerr',1,'CE');
 
