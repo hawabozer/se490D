8c7315ecfb17ba05b19d0ecd1eb6ae0f103a4429
hawabozer <hawaabozer@gmail.com>
2022-07-27 11:34:34 -0700

    k

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -2,5 +2,5 @@ INSERT INTO Department(Name,CollgID,Code)
 VALUES ('software enginerr',1,'CN');
 SELECT * FROM Department
 insert into department(Name,collgID,code)
-VALUES ('software enginerr',1,'CE');
+VALUES ('software enginerr',1,'Ceeee');
 
